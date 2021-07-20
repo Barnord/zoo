88241140b9cc30441fb7eb183c28886ea52e221e
barnord <bdarno92@gmail.com>
2021-07-19 20:50:43 -0700

    halfway through making the class structures.

---
+++ zoo/zoo/Classes/Cetaceans.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zoo.Classes
+{
+    public abstract class Cetaceans : Mammals
+    {
+        public override int legs { get; set; } = 0;
+        public virtual bool aquatic { get; set; } = true;
+        public virtual string diet { get; set; } = "carnivorous";
+    }
+    }
+}
